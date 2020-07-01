a8221f6e01befcafbe5355bc00ae6efc9b40c031
Zyleaf <Zyleaff@gmail.com>
2020-07-01 11:54:39 -0400

    Fixed spelling

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -3,5 +3,4 @@ const updateStatus = require('../../snippets/readyEvent/updateStatus');
 module.exports = async (client) => {
     console.log('Bot online!');
     updateStatus(client);
-    client.user.setStatus('dnd');
 };
